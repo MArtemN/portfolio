cc898ae0c8ceefa0dee6f1626dedca70a443ac73
ArtemM <martemn71@gmail.com>
2022-04-15 18:04:08 +0300

    edit mail scipt

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -8,13 +8,18 @@ $check = $_POST['check'];
 if ($name && $email && $message && $check) {
 	$to = 'ma@webvibe.ru';
 	$subject = 'Новая заявка с формы';
+    $headers = array(
+        'From' => 'ma@webvibe.ru',
+        'Reply-To' => 'ma@webvibe.ru',
+        'X-Mailer' => 'PHP/' . phpversion()
+    );
 	$message = '
 		Новая заявка с формы
 		Имя: ' . $name . '
 		E-mail: ' . $email . '
 		Сообщение: ' . $message;
 		
-	if (!mail($to, $subject, $message)) {
+	if (!mail($to, $subject, $message, $headers)) {
 		return false;
 	}
 }
