e48db9fe89e6b2d66a69b4f9cdcbd5bfe6c57101
ArtemM <martemn71@gmail.com>
2022-06-06 16:43:34 +0300

    create repo for production

--- dist/mailer/smart.php
+++ dist/mailer/smart.php
@@ -8,15 +8,27 @@ $check = $_POST['check'];
 if ($name && $email && $message && $check) {
 	$to = 'ma@webvibe.ru';
 	$subject = 'Новая заявка с формы';
-    $headers = array(
-        'From' => 'Артем Матюхин <ma@webvibe.ru>',
-        'Bcc' => 'martemn71@gmail.com'
-    );
+    $charset = "utf-8";
+    $headers ="Content-type: text/html; charset=$charset\r\n";
+    $headers.="Date: ".date('D, d M Y h:i:s')."\r\n";
+    $headers.="From: Артем Матюхин <ma@webvibe.ru>\r\n";
+    $headers.="Bcc: martemn71@gmail.com\r\n";
+//    $headers = array(
+//        'From' => 'Артем Матюхин <ma@webvibe.ru>',
+//        'Bcc' => 'martemn71@gmail.com'
+//    );
 	$message = '
-		Новая заявка с формы
-		Имя: ' . $name . '
-		E-mail: ' . $email . '
-		Сообщение: ' . $message;
+	    <html>
+	        <head>
+	            <title>'.$subject.'</title>
+            </head>
+            <body>
+                Новая заявка с формы <br><br>
+                Имя: ' . $name . ' <br>
+                E-mail: ' . $email . ' <br>
+                Сообщение: ' . $message . '
+            </body>
+	    </html>';
 		
 	if (!mail($to, $subject, $message, $headers)) {
 		return false;
